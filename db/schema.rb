1998a70719c8fafc95ec1574d507c7905b447c43
Naga <nno7.okt@gmail.com>
2021-11-23 02:59:01 +0900

    to use psql(comp)

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_20_193334) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "error_lists", force: :cascade do |t|
     t.string "err_mess"
     t.datetime "created_at", precision: 6, null: false
