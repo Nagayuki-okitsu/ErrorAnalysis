07570d0520f1028c631957ff128d106399e187b8
Naga <nno7.okt@gmail.com>
2021-12-23 19:26:59 +0900

    migrationファイル修正

--- db/migrate/20211223093734_drop_table_delete_answers.rb
+++ db/migrate/20211223093734_drop_table_delete_answers.rb
@@ -1,9 +1,4 @@
 class DropTableDeleteAnswers < ActiveRecord::Migration[6.1]
   def change
-    drop_table :delete_answers do |t|
-      t.string "delete_answer_id"
-      t.bigint "user_id"
-      t.timestamps null: false
-    end
   end
 end
--- db/migrate/20211223093755_drop_table_posts.rb
+++ db/migrate/20211223093755_drop_table_posts.rb
@@ -1,8 +1,4 @@
 class DropTablePosts < ActiveRecord::Migration[6.1]
   def change
-    drop_table :posts do |t|
-      t.bigint "user_id"
-      t.timestamps null: false
-    end
   end
 end
