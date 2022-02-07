e3f0b26355b4cb02c30db4cebde66acf515b3c0a
Naga <nno7.okt@gmail.com>
2022-02-07 17:47:34 +0900

    学習機能v1調整

--- app/controllers/learns_controller.rb
+++ app/controllers/learns_controller.rb
@@ -1,13 +1,15 @@
 class LearnsController < ApplicationController
     def index
+        session[:status] = "new"
+        flash[:mess] = "選択肢の紫バーをつかむとボックスを移動できます"
     end
 
     def test
-        #session[:q_arr_index] = nil
-        #session[:status] = ""
-
-        if params[:difc].present? && session[:status] != "new"
-            session[:status] = "new"
+        
+        if params[:difc].present? && session[:status] == "new"
+            session[:status] = "stay_c"
+            session[:q_arr_index] = nil
+            session[:ext_q] = nil
             session[:difc] = params[:difc]
             session[:kind] = params[:kind]
             session[:total] = params[:total].to_i
@@ -15,13 +17,14 @@ class LearnsController < ApplicationController
         
         learn_arr = open("#{Rails.root}/app/views/learns/learn.yml", "r") { |f| YAML.load(f) } 
 
-        if session[:kind] == "nothing"
-            learn_arr = custom_q(learn_arr,"difc",session[:difc])
+        #カスタムとマッチする問題を抽出する
+        if session[:kind] == "nothing"  
+            learn_arr = custom_q(learn_arr,"difc",session[:difc]) #種類を指定しない場合は難易度のみマッチする問題を抽出
         else
-            learn_arr = custom_q(custom_q(learn_arr,"difc",session[:difc]),"kind",session[:kind])
+            learn_arr = custom_q(custom_q(learn_arr,"difc",session[:difc]),"kind",session[:kind]) #種類と難易度がマッチする問題を抽出
         end
 
-        if params[:status].present?
+        if params[:status].present? #問題の進捗状況を制御する処理(ブラウザを更新されても内容が変わらないようにする)
             if (session[:status] != "stay_s" && session[:status] != "stay_c") || ( session[:status] == "stay_s" && params[:status] == "change" ) || ( session[:status] == "stay_c" && params[:status] == "next" ) 
                 session[:status] = params[:status]
             end
@@ -29,9 +32,9 @@ class LearnsController < ApplicationController
 
         if session[:status] != "finish"                                                                                                 #「 session[:status]の追跡 」 
 
-            if session[:q_arr_index].nil?                                                                                               #  開始時　・・・ new                                                      
+            if session[:q_arr_index].nil?                                                                                               #  開始時　・・・ new → stay_c                                               
                 session[:number] = 0                                                                                                    #　  　↓
-                session[:q_arr_index] = select_q(learn_arr,session[:total])                                                             #  原因から解決 ・・・ next ①
+                session[:q_arr_index] = select_q(learn_arr.length-1,session[:total])                                                    #  原因から解決 ・・・ next ①
                 session[:r_ans] = nil                                                                                                   #　  　↓
                 session[:q_order] = nil                                                                                                 #（解決選択 ・・・ stay_s）②
                 session[:check] = nil                                                                                                   #  　　↓
@@ -40,7 +43,7 @@ class LearnsController < ApplicationController
             @key = ""                                                                                                                   #　  　↓
             if session[:status] == "next" || session[:status] == "change"                                                               # (原因選択 ・・・ stay_c) ①に戻る
 
-                if session[:status] == "next"                                                                                           #  　　↓
+                if session[:status] == "next"                                                                                            
                     @key = "s_a"                                                                                                        #  ②から終了時 ・・・ finish
                     session[:status] = "stay_s"
                 else 
@@ -63,30 +66,25 @@ class LearnsController < ApplicationController
                 session[:r_ans], session[:q_order] = shuffle_q #問題の選択肢をシャッフルする
             end
 
-            q = learn_arr[session[:q_arr_index][session[:number]]] #問題を抽出する
-
-            @q = order_q(q,session[:q_order],"#{@key}") #抽出した問題を並べ替えてインスタンスとする
-
-            if params[:ans].present?
+            if params[:ans].present? #回答が送られたら答え合わせ
                 session[:check] = check(session[:r_ans],params[:ans])
 
                 if session[:number] + 1 == session[:total] && session[:status] == "stay_s"
-                    session[:status] = "finish"
-                    tmp = session[:q_arr_index]
-                    session[:tmp_q_index] = tmp[tmp.length-1]
-                    session[:q_arr_index] = nil
+                    session[:status] = "finish" #全ての問題が終了したら進捗状況をfinishにする
                 end
+
             end
         
         else
-            q = learn_arr[session[:tmp_q_index][session[:number]]] #問題を抽出する
-            @q = order_q(q,session[:q_order],"s_a") #抽出した問題を並べ替えてインスタンスとする
-            @key = "s_a"
+            @key = "s_a" 
         end
+
+        q = learn_arr[session[:q_arr_index][session[:number]]] #問題を抽出する
+        @q = order_q(q,session[:q_order],"#{@key}") #抽出した問題を並べ替えてインスタンスとする
 
     end
 
-    def custom_q(learn_arr,key,val)
+    def custom_q(learn_arr,key,val) #カスタム通りの問題を作成するメソッド
 
         new_learn_arr = []
         
@@ -100,28 +98,28 @@ class LearnsController < ApplicationController
 
     end
 
-    def select_q(learn_arr, total)
+    def select_q(q_total, total) #問題をランダムに選ぶための添え字を作成するメソッド
 
-        q_arr_index = []
-        q_arr_index.push(rand(0..(learn_arr.length-1)))
+        q_arr_index = [-1]
 
-        (total-1).times do
+        for i in 0..(total-1)
 
             flg = false
-            i = 0
+            r = 0
 
             while flg == false
-                i = rand(0..(learn_arr.length-1))
+                r = rand(0..q_total)
+                flg = true
 
                 for j in 0..(q_arr_index.length-1)
-                    if i == q_arr_index[j]
+                    if r == q_arr_index[j]
+                        flg = false
                         break
                     end
-                    flg = true
                 end
             end
 
-            q_arr_index.push(i)
+            q_arr_index[i] = r
 
         end
 
@@ -129,12 +127,12 @@ class LearnsController < ApplicationController
 
     end
 
-    def shuffle_q #選択肢の配列の添え字をシャッフルする
+    def shuffle_q #選択肢の配列の添え字をシャッフルするメソッド
 
         r_ans = 1 #問題の答えが格納されている場所を記憶する（各問題の答えは選択肢の1つ目に格納されている）
         q_order = [0,1,2,3] 
 
-        3.times do
+        3.times do #3回シャッフル
 
             i = rand(0..3)
             j = rand(0..3)
@@ -158,7 +156,7 @@ class LearnsController < ApplicationController
         return r_ans, q_order
     end
 
-    def order_q(q,order,key)
+    def order_q(q,order,key) #シャッフルした順番通りに選択肢を並べ替えるメソッド
 
         tmp_q = []
 
@@ -173,11 +171,11 @@ class LearnsController < ApplicationController
         return q
     end
 
-    def check(r_ans,ans)
+    def check(r_ans,ans) #答え合わせをするメソッド
 
         check = "mistake"
         if r_ans == 1 && ans == "a" || r_ans == 2 && ans == "b" || r_ans == 3 && ans == "c" || r_ans == 4 && ans == "d" 
-            check = "correct"
+            check = "correct" #r_ans=正解の場所が格納してある(abcd=1234) ans=送られてきた答え
         end
 
         return check
